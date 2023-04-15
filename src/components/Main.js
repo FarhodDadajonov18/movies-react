914dad117b18b52bfc63e5c9bb9f3b5137905046
dadajonovfarhod <dadajonovfarhod5@gmail.com>
2023-04-15 09:35:51 +0500

    changed in  index.css file  search design

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import Movies from './Movies'
-import Loader from './Loader'
-import Search from './Search'
+import React, { useEffect, useState } from "react";
+import Movies from "./Movies";
+import Loader from "./Loader";
+import Search from "./Search";
 
 export default function Main() {
- 
-  const [movies , setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
 
-
-  const searchMovies = (str, type='all') =>{
-    setLoading(true)
-    fetch(`https://www.omdbapi.com/?apikey=af97bad&s=${str}${type !== 'all' ?  `&type=${type}` : ''}`)
-    .then((response) => response.json())
-    .then((data) =>{ setMovies(data.Search);
-                     setLoading(false);
-        }); 
+  const searchMovies = (str, type = "all") => {
+    setLoading(true);
+    fetch(
+      `https://www.omdbapi.com/?apikey=af97bad&s=${str}${
+        type !== "all" ? `&type=${type}` : ""
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      });
   };
 
-  useEffect(()=>{
-     setLoading(true)
+  useEffect(() => {
+    setLoading(true);
     fetch("https://www.omdbapi.com/?apikey=af97bad&s=panda")
-    .then((response)=> response.json())
-    .then((data)=>{ setMovies(data.Search);
-                    setLoading(false);
-        });
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      });
   }, []);
 
- 
+  return (
+    <div className="container content">
+      <Search searchMovies={searchMovies} />
 
-    return (
-      <div className='container content'>
-          <Search searchMovies = {searchMovies} />
-
-        {loading ? (<Loader/>) : (<Movies movies = {movies} />) }
-         
-      </div>
-    );
-  
+      {loading ? <Loader /> : <Movies movies={movies} />}
+    </div>
+  );
 }
-
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,76 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
- function Search ({ searchMovies }) {
+function Search({ searchMovies }) {
+  const [search, setSearch] = useState("panda");
+  const [type, setType] = useState("all");
 
-  const [search , setSearch] = useState("panda");
-  const [type , setType ] = useState("all");
+  const handleKey = (e) => {
+    if (e.key === "Enter") {
+      searchMovies(search, type);
+    }
+  };
 
+  const handleFilter = (e) => {
+    setType(e.target.dataset.type);
+    searchMovies(search, e.target.dataset.type);
+  };
 
-const handleKey = (e) => {
-  if(e.key === 'Enter'){
-    searchMovies(search,type);
-  }
-}
-
-const  handleFilter = (e) =>{
-  setType(e.target.dataset.type)
-  searchMovies(search, e.target.dataset.type)
-}  
- 
-    return (
-      <div className="row">
-        <div className="input-field">
-          <input 
-            id="email_inline" 
-            type="search" 
-            placeholder="Search" 
-            autoComplete='off'
-            className="validate"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            onKeyDown= {handleKey}
-         />
-         <button className='btn' onClick={()=>searchMovies(search,type)}>Search</button>
+  return (
+    <div className="row">
+      <div className="input-field">
+        <input
+          id="email_inline"
+          type="search"
+          placeholder="Search"
+          autoComplete="off"
+          className="validate"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={handleKey}
+        />
+        <button className="btn" onClick={() => searchMovies(search, type)}>
+          Search
+        </button>
       </div>
-      
-      <div> 
-          <label>
-            <input className="with-gap"
-                   name="type"
-                   type="radio" 
-                   data-type='all' 
-                   checked={type === 'all'}
-                   onChange={handleFilter}
-                />
-                   <span>All</span>
-          </label>
 
-          <label>
-            <input className="with-gap"
-                   name="type"
-                   type="radio" 
-                   data-type='movie'
-                   checked={type ==='movie'}
-                   onChange={handleFilter}
-                />
-                    <span>Movies only</span>
-          </label>
+      <div>
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="all"
+            checked={type === "all"}
+            onChange={handleFilter}
+          />
+          <span>All</span>
+        </label>
 
-          <label>
-            <input className="with-gap"
-                   name="type"
-                   type="radio" 
-                   data-type='series' 
-                   checked={type === 'series'}
-                   onChange={handleFilter}
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="movie"
+            checked={type === "movie"}
+            onChange={handleFilter}
+          />
+          <span>Movies only</span>
+        </label>
 
-                />
-                   <span>Series only</span>
-          </label>
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="series"
+            checked={type === "series"}
+            onChange={handleFilter}
+          />
+          <span>Series only</span>
+        </label>
       </div>
     </div>
-    )
-  
+  );
 }
 
 export default Search;
